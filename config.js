ad04caeb00229496e893b09809925d9cb0cc9517
Oliver Oszko <oliver@oszko.net>
2015-03-31 21:09:59 +0200

    copy from Grid3d

---
+++ config.js
@@ -0,0 +1,16 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "three": "github:mrdoob/three.js@master",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+  }
+});
+
